01096078b51b682e47559b9ea2a9445339c8550c
shradhakatyal <skatyal2204@gmail.com>
2020-06-21 19:54:00 +0530

    initial setup, basic design done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PlayerInfo from './components/PlayerInfo/PlayerInfo';
+import GameBoard from './components/GameBoard/GameBoard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isActive: 'x',
+      boardState: [['', '', ''], ['', '', ''], ['', '', '']]
+    }
+  }
+
+  clickOnCell = (e, i, j) => {
+    console.log(i, j);
+    if(!this.state.boardState[i][j]) {
+      console.log('here')
+      this.setState((prevState) => {
+        const tempArr = prevState.boardState;
+        tempArr[i][j] = prevState.isActive;
+        console.log(tempArr);
+        return {
+          boardState: tempArr,
+          isActive: prevState.isActive === 'x' ? 'o' : 'x',
+        }
+      }, () => {
+        console.log(this.state.boardState)
+      });
+    }
+  }
+
+  render() {
+    const { isActive, boardState } = this.state;
+    return (
+      <div className="main-wrapper">
+        <h1 className='main-heading'>TIC TAC TOE</h1>
+        <PlayerInfo isActive={isActive} />
+        <GameBoard isActive={isActive} boardState={boardState} clickOnCell={this.clickOnCell} />
+      </div>
+    );
+  }
 }
 
 export default App;
