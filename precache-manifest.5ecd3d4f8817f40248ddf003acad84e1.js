1934ee63355974dab34b01d05e4fb405762126ef
shradhakatyal <skatyal2204@gmail.com>
2020-06-21 23:14:38 +0530

    Updates

---
+++ precache-manifest.5ecd3d4f8817f40248ddf003acad84e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dbd5e23ed34158f4ca0841ac4941779",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "3f287ddcb7bb648d95b3",
+    "url": "/TicTacToe/static/css/main.59b6e09c.chunk.css"
+  },
+  {
+    "revision": "3096af9771085dd4b06c",
+    "url": "/TicTacToe/static/js/2.368069f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe/static/js/2.368069f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f287ddcb7bb648d95b3",
+    "url": "/TicTacToe/static/js/main.702f19b9.chunk.js"
+  },
+  {
+    "revision": "ebdbc3f3c36f0e8a7009",
+    "url": "/TicTacToe/static/js/runtime-main.3e2f971d.js"
+  }
+]);
