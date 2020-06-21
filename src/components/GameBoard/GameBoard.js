af88cb24b592999d24691cd819520bc2e2e6dcfa
shradhakatyal <skatyal2204@gmail.com>
2020-06-21 23:10:05 +0530

    comments removed

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -34,19 +34,3 @@ const GameBoard = ({ isActive, boardState, onClickCell }) => {
 }
 
 export default GameBoard;
-
-{/* <div className='row'>
-          <div className='cell' onClick={(e) => clickOnCell(e, 0, 0)}>{boardState[0][0] === 'x' ? <span>&times;</span> : <span>&#9675;</span>}</div>
-          <div className='cell' onClick={(e) => clickOnCell(e, 0, 1)}>{boardState[0][1] === 'x' ? <span>&times;</span> : <span>&#9675;</span>}</div>
-          <div className='cell' onClick={(e) => clickOnCell(e, 0, 2)}>{boardState[0][2] === 'x' ? <span>&times;</span> : <span>&#9675;</span>}</div>
-        </div>
-        <div className='row'>
-          <div className='cell' onClick={(e) => clickOnCell(e, 1, 0)}>{boardState[1][0] === 'x' ? <span>&times;</span> : <span>&#9675;</span>}</div>
-          <div className='cell' onClick={(e) => clickOnCell(e, 1, 1)}>{boardState[1][1] === 'x' ? <span>&times;</span> : <span>&#9675;</span>}</div>
-          <div className='cell' onClick={(e) => clickOnCell(e, 1, 2)}>{boardState[1][2] === 'x' ? <span>&times;</span> : <span>&#9675;</span>}</div>
-        </div>
-        <div className='row'>
-          <div className='cell' onClick={(e) => clickOnCell(e, 2, 0)}>{boardState[2][0] === 'x' ? <span>&times;</span> : <span>&#9675;</span>}</div>
-          <div className='cell' onClick={(e) => clickOnCell(e, 2, 1)}>{boardState[2][1] === 'x' ? <span>&times;</span> : <span>&#9675;</span>}</div>
-          <div className='cell' onClick={(e) => clickOnCell(e, 2, 2)}>{boardState[2][2] === 'x' ? <span>&times;</span> : <span>&#9675;</span>}</div>
-        </div> */}
